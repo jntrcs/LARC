204cad551b3edb31b0341ec0bd8d69f51a90e7fc
Jackson Curtis <jcurtis210@gmail.com>
2016-09-14 16:11:08 -0600

    Deleted NBA analysis because it was just a repeat of datascrape. Finished Mosteller analysis on NCAAF.

--- Code/Final Code update May_17_16/NCAAFAnalysis.R
+++ Code/Final Code update May_17_16/NCAAFAnalysis.R
@@ -4,6 +4,7 @@
 
 load("NCAAWeek2Configured.RData")
 load("BTResultsWeek2.RData")
+load("'MTResultsWeek2.RData")
 
 #ncaadata<-datascrape("NCAAF")
 #head(ncaadata)
@@ -14,8 +15,12 @@ load("BTResultsWeek2.RData")
 #system.time(
 #bradter<-LARC.Rank(football))
 system.time(
-Mosteller<-LARC.Rank(football, func = ThurstoneMostellerLARC))
+Mosteller<-LARC.Rank(week2Configured, increment=.01, func = ThurstoneMostellerLARC)) #11 minutes at .01 accuracy
 #BradleyTerryRank<-bradter
 #week2Configured<-football
 #save(week2Configured, file="NCAAWeek2Configured.RData")
 #save(BradleyTerryRank, file="BTResultsWeek2.RData")
+#Mosteller
+#MTNCAAFweek2<-Mosteller
+#save(MTNCAAFweek2, file="MTResultsWeek2.RData")
+cbind(MTNCAAFweek2, BradleyTerryRank)
