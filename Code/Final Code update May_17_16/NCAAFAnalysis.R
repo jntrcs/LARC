745874efa514f449e9802580836555202bfa4d1b
Jackson Curtis <jcurtis210@gmail.com>
2016-09-22 17:49:44 -0600

    Fixed the week 2 data to match formatting and to include the Hawaii-California game

--- Code/Final Code update May_17_16/NCAAFAnalysis.R
+++ Code/Final Code update May_17_16/NCAAFAnalysis.R
@@ -1,8 +1,11 @@
 #NCAA week by week
 
-#An analysis file that will pull in football results and run them through LARC Rank
-
-
+#This script will configure the data and generate the strengths after each week of the football season.
+sum(week2Configured[week2Configured$Team=="Hawaii", 4])
+week2Configured[week2Configured$Team=="California", 1:3]
+BradleyTerryRank[BradleyTerryRank$Team=="California",]
+BTResultsWeek3[BTResultsWeek3$Team=="Hawaii",]
+BradleyTerryRank[BradleyTerryRank$Team=="Hawaii",]
 
 #WEEK 3
 week3<-dataconfigure(datascrape("NCAAF"), reldate = as.Date("2016-09-18"))
@@ -16,29 +19,17 @@ system.time(
 save(BTResultsWeek3, TMResultsWeek3, file="Week3Results.RData")
 
 #WEEK 2
-#load("BTResultsWeek2.RData")
-#load("MTResultsWeek2.RData")
-#load("NCAAWeek2Configured.RData")
 
-
-#ncaadata<-datascrape("NCAAF")
-#head(ncaadata)
-#system.time(
-#football<-dataconfigure(ncaadata)
-#)
-#head(football)
-#system.time(
-#BTWeek2<-LARC.Rank(week2Configured)
-#system.time(
-#TMWeek2<-LARC.Rank(week2Configured, increment=.01, func = ThurstoneMostellerLARC) #11 minutes at .01 accuracy
-#save(BRWeek2, TMWeek2, file="Week2Results.RData")
-#week2Configured<-football
-#save(week2Configured, file="NCAAWeek2Configured.RData")
-#save(BradleyTerryRank, file="BTResultsWeek2.RData")
-#Mosteller
-#MTNCAAFweek2<-Mosteller
-#save(MTNCAAFweek2, file="MTResultsWeek2.RData")
-#cbind(MTNCAAFweek2, BradleyTerryRank)
+week2<-week2Configured
+#week2<-dataconfigure(datascrape("NCAAF"), reldate = as.Date("2016-09-11"))
+save(week2, file="NCAAFWeek2Configured.Rdata")
+system.time(
+  BTResultsWeek2<-LARC.Rank(week2)
+)
+system.time(
+  TMResultsWeek2<-LARC.Rank(week2, increment = .01, func=ThurstoneMostellerLARC)
+)
+save(BTResultsWeek2, TMResultsWeek2, file="Week2Results.RData")
 
 #WEEK 1
 #load("NCAAWeek1Configured.RData")
@@ -55,21 +46,6 @@ TMResultsWeek1<-LARC.Rank(week1, increment = .01, func=ThurstoneMostellerLARC)
 save(BTResultsWeek1, TMResultsWeek1, file="Week1Results.RData")
 #cbind(BTResultsWeek1, TMResultsWeek1)
 
-date<-"2016-09-19"
-weekNumber<-3
-buildWeek<-function(weekNumber, date) #Takes a date in form "YYYY-MM-DD" and scrapes the data, configures, and runs both ranking systems on it. Saves the results
-  #this is to be called at the start of each week for the last week
-{
-  if (file.exists("FootballData.RData"))
-    load("FootballData.RData")
-  if (!exists("footballdata"))
-    footballdata<-list()
-  if (is.null(footballdata[[weekNumber]]))
-  {
-    footballdata[[weekNumber]]<-dataconfigure(datascrape("NCAAF"), reldate=date)
-    save(footballdata, file="FootballData.RData")
-  }
-  
-}
+
 
  
