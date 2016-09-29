60ab6b918a2eb3809dc37e47ceb15856c208c006
Jackson Curtis <jcurtis210@gmail.com>
2016-09-29 08:39:21 -0600

    The results from the supercomputer rerunning all data

--- Code/Final Code update May_17_16/NCAAFAnalysis.R
+++ Code/Final Code update May_17_16/NCAAFAnalysis.R
@@ -6,7 +6,7 @@ load("LatestNCAAF.RData")
 #WEEK 4
 week4<-dataconfigure(latestRaw, reldate = as.Date("2016-09-25"))
 save(week4, file="NCAAFWeek4Configured.Rdata")
-print(head(week4[1:2,]))
+
 system.time(
   BTResultsWeek4<-LARC.Rank(week4)
 )
