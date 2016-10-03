d3a809549b3a13fe3df937f0bfe18a8eb0443790
Jackson Curtis <jcurtis210@gmail.com>
2016-10-03 10:34:08 -0600

    Week 5 results

--- Code/Final Code update May_17_16/NCAAFAnalysis.R
+++ Code/Final Code update May_17_16/NCAAFAnalysis.R
@@ -4,7 +4,7 @@ load("MasterFunctionFile.RData")
 load("LatestNCAAF.RData")
 
 #Week 5
-week5<-dataconfigure(latestRaw, relDate=as.Date("2016-10-02"))
+week5<-dataconfigure(latestRaw, reldate=as.Date("2016-10-02"))
 save(week5, file="NCAAFWeek5Configured.RData")
 system.time(
   BTResultsWeek5<-LARC.Rank(week5)
