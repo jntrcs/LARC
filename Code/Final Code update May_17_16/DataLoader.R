a2b994a0bdc2ff08891b835eb43f541fa0b4e831
Jackson Curtis <jcurtis210@gmail.com>
2016-10-05 16:08:38 -0600

    Provided an abstraction to make fetching data easier without knowing the data structure

--- Code/Final Code update May_17_16/DataLoader.R
+++ Code/Final Code update May_17_16/DataLoader.R
@@ -5,6 +5,12 @@ latestRaw<-datascrape("NCAAF")
 
 save(all2016data,latestRaw,file="2016FootballData.RData") 
 
+#This exists because the supercomputer could quite easily overwrite my existing work and I want to 
+#keep this easily available, also will be able to use for before and after comparisons
+all2016dataCopy<-all2016data
+latestRawCopy<-latestRaw
+save(all2016data, latestRaw, file="2016copy.RData")
+
 #The structure will be as follows: each item in the first list corresponds to what week it relates too. 
 #eg. all2016data[[1]] refers to week ones data
 #Each item is a sublist containing these items:
@@ -23,3 +29,9 @@ all2016data<-list(list(week1, BTResultsWeek1, TMResultsWeek1),
 all2015data<-list()
 raw2015<-datascrape("NCAAF", year=2015)
 save(all2015data, raw2015, file="2015FootballData.RData")
+
+all2015dataCopy<-all2015data
+raw2015Copy<-raw2015
+save(all2015dataCopy, raw2015Copy, file="2015Copy.Rdata")
+
+rm(all2015dataCopy, all2016dataCopy, raw2015Copy, latestRawCopy)
