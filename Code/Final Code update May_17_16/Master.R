8a0b48046595186e83b1b510c410a89ba1ed0b17
Jackson Curtis <jcurtis210@gmail.com>
2016-11-05 23:07:56 -0600

    Started developing the outline of the schedule

--- Code/Final Code update May_17_16/Master.R
+++ Code/Final Code update May_17_16/Master.R
@@ -4,13 +4,14 @@
 #Whenever a function is updated, this function NEEDS to be called to update the master file
 save(BradleyTerryLARC, BTWP, dataconfigure, datascrape, find.mf, full.sim.gen, LARC.Compare, LARC.Optim,
      LARC.Posterior, LARC.Prob, LARC.Rank, print.prthdn, progresspredict.simulation, progresspredict, scrapeNBA,
-     scrapeNCAAB, scrapeNCAAF, scrapeNFL, scrapeNHL, sim.gen, simulate, simulation, ThurstoneMostellerLARC,
+     scrapeNCAAB, scrapeNCAAF, scrapeNFL, scrapeNHL, sim.gen, ThurstoneMostellerLARC,
      TMWP, VS.matrix, NCAAFPredictor, makePenaltyGraph, makePerformanceGraph,
      makeDifferenceGraph, getConfigured, getRaw, getBTStrengths, getTMStrengths,
      getStrengths, getPredictions, LARC.Rank.Football,attachMostRecentStrengths, TMDensity, BTDensity,
-     pickTMStrength, pickBTStrength, simHomeWin,
+     pickTMStrength, pickBTStrength, simHomeWin, generateTeams, generateSchedule, generateConference,
+     generateNonConference,
      file="MasterFunctionFile.RData")
-
+#Removed: simulate, simulation,
 
 #Whenevere you need to load functions, call this command
 load("MasterFunctionFile.RData")
--- Code/Final Code update May_17_16/SimulationSetUp.R
+++ Code/Final Code update May_17_16/SimulationSetUp.R
@@ -2,28 +2,16 @@
 numSim <-10 #Change this to 1000
 
 simulation<-list()
-simulation[["Type"]]<-"BT"
-simulation
 
-
-
-generateSeason<-function(type)
+for (i in 1:numSim)
 {
-  if(type=="BT")
-  {
-    params<-c(2,3,1.5,2.5,1,2,4,.5,1.25,.75)
-    func<-pickBTStrength
-  }
-  else
-  {
-    params<-c(0,1,-.5,.5,-1,0,2,-1.5,-.75, -1.25)
-    func<-pickTMStrength
-  }
+  ##idea: change all of these to not store in i so that it doesn't save all info for 1000 sims
+ simulation[[i]]<-list()
+  simulation[[i]]$Teams<-generateTeams()
+  simulation[[i]]$Schedule<-generateSchedule()
   
-  teams<-data.frame(rep(1:10, each=9), 1:90, rep(0, 90))
-  names(teams)<-c("Conference", "Team", "TrueStrength")
-  teams$TrueStrength<-func(params[teams$Conference])
-  teams
 }
 
+
+generateTeams()
 save(simulation, "Simulation.RData")
