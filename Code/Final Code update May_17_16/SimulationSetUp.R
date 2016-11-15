a300ee47b6eac35d0035ae2690f34621d9eb008f
Jackson Curtis <jcurtis210@gmail.com>
2016-11-15 15:52:48 -0700

    Prepped for simulation on supercomp

--- Code/Final Code update May_17_16/SimulationSetUp.R
+++ Code/Final Code update May_17_16/SimulationSetUp.R
@@ -1,19 +1,15 @@
-##Simulation set up
-numSim <-10 #Change this to 1000
+##Simulation function
 
-simulation<-list()
-
-for (i in 1:numSim)
-{
-  ##idea: change all of these to not store in i so that it doesn't save all info for 1000 sims
- simulation[[i]]<-list()
-  simulation[[i]]$teamSchedule<-generateTeamSchedule()
-  simulation[[i]]$BTseasonGames<-generateSeasonResults(simulation[[i]]$teamSchedule, useBT = TRUE)
-  simulation[[i]]$TMseasonGames<-generateSeasonResults(simulation[[i]]$teamSchedule, useBT=FALSE)
-  
+simulate1<-function()
+  simulation<-list()
+  simulation$teamSchedule<-generateTeamSchedule()
+  simulation$BTseasonGames<-generateSeasonResults(simulation[[i]]$teamSchedule, useBT = TRUE)
+  simulation$TMseasonGames<-generateSeasonResults(simulation[[i]]$teamSchedule, useBT=FALSE)
+  for (i in 1:13)
+  {
+    configured<-dataconfigure()
+  }
 }
 
-a<-dataconfigure(simulation[[i]]$BTseasonGames, reldate= 13)
 
-save(simulation, file="Simulation.RData")
 
---
+++ Code/Final Code update May_17_16/SupercomputerSim.R
@@ -0,0 +1,18 @@
+#The file that will actually be submitted to the supercomputer to run
+
+library(Rcpp)
+load("MasterFunctionFile.RData")
+Rcpp::sourceCpp("cppFiles.cpp")
+
+library(parallel)
+numCores<-17
+clust <- makeCluster(numCores)
+neededFunc<- c("dataconfigure", "LARC.Rank.Football", "BTDensity", "TMDensity",
+               "ThurstoneMostellerLARC",   "BradleyTerryLARC", "LARC.Rank", "LARC.Optim",
+               "find.mf","attachMostRecentStrengths", "TMDensity", "BTDensity",
+               "pickTMStrength", "pickBTStrength", "simHomeWin", "generateTeams","generateSchedule", 
+               "generateConference","generateNonConference", "generateTeamSchedule", "generateSeasonResults")
+clusterExport(clust, neededFunc)
+
+output<- parLapply(clust, 1:10, fun = function(i){simulate1()})
+save(output, file="Simulation.RData")
