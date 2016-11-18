0ccd0ebe580b33c7368e55d0d6596c8c439d7792
Jackson Curtis <jcurtis210@gmail.com>
2016-11-17 21:00:17 -0700

    working on simulation output

--- Code/Final Code update May_17_16/Master.R
+++ Code/Final Code update May_17_16/Master.R
@@ -9,7 +9,7 @@ save(BradleyTerryLARC, BTWP, dataconfigure, datascrape, find.mf, full.sim.gen, L
      makeDifferenceGraph, getConfigured, getRaw, getBTStrengths, getTMStrengths,
      getStrengths, getPredictions, LARC.Rank.Football,attachMostRecentStrengths, TMDensity, BTDensity,
      pickTMStrength, pickBTStrength, simHomeWin, generateTeams, generateSchedule, generateConference,
-     generateNonConference, generateTeamSchedule, generateSeasonResults, simulate1,
+     generateNonConference, generateTeamSchedule, generateSeasonResults, simulate1, normalizeSample,
      file="MasterFunctionFile.RData")
 #Removed: simulate, simulation,
 
--- Code/Final Code update May_17_16/SimulationSetUp.R
+++ Code/Final Code update May_17_16/SimulationSetUp.R
@@ -1,5 +1,6 @@
 ##Simulation function
-
+useBT<-FALSE
+Rcpp::sourceCpp("cppFiles.cpp")
 simulate1<-function(useBT)
 {
   simulation<-list()
@@ -13,11 +14,36 @@ simulate1<-function(useBT)
     if (i!=1)
       configured<-attachMostRecentStrengths(configured, strengths[[i-1]]$BT, strengths[[i-1]]$TM)
     strengths[[i]]<-list()
-    strengths[[i]]$BT<-LARC.Rank.Football(configured, func=BTDensity)
-    strengths[[i]]$TM<-LARC.Rank.Football(configured, func=TMDensity)
+    strengths[[i]]$BT<-LARC.Rank.Football(configured, func=BTDensity, sort=FALSE)
+    strengths[[i]]$TM<-LARC.Rank.Football(configured, func=TMDensity, sort=FALSE)
   }
-  strengths
+  normTrueStrengths<-normalizeSample(simulation$teamSchedule$TrueStrength)
+  #Should the mean strength be the sample or the population
+  #Same with SD
+  summaryOfResults<-list()
+  for (i in 1:13)
+  {
+    summaryOfResults[[i]]<-list()
+    weekBT<-normalizeSample(strengths[[i]]$BT$Strength)
+    weekTM<-normalizeSample(strengths[[i]]$TM$Strength)
+    summaryOfResults[[i]]$BTBias<-mean(sqrt((normTrueStrengths-weekBT)^2))
+    summaryOfResults[[i]]$TMBias<-mean(sqrt((normTrueStrengths-weekTM)^2))
+    
+  }
 }
 
+normalizeSample<-function(strengths)
+{
+  meanStrength<-mean(strengths)
+  sdStrength<-sd(strengths)
+  norm <- (strengths- meanStrength)/sdStrength
+  norm
+}
+
+plot(weekBT, normTrueStrengths)
+plot(weekTM, normTrueStrengths)
+summaryOfResults
 
 
+mean(.4<(abs(.5-simulation$seasonGames$HomeWinPerecent)))
+hist(abs(.5-simulation$seasonGames$HomeWinPerecent))
