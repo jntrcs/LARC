0a415ad7fe88928d2936803ee9c46affae365e91
Jackson Curtis <jcurtis210@gmail.com>
2016-12-22 12:24:11 -0700

    supercomp stuff

--- Code/Final Code update May_17_16/SupercomputerSim.R
+++ Code/Final Code update May_17_16/SupercomputerSim.R
@@ -10,9 +10,9 @@ neededFunc<- c("dataconfigure", "LARC.Rank.Football", "BTDensity", "TMDensity",
                "ThurstoneMostellerLARC",   "BradleyTerryLARC", "LARC.Rank", "LARC.Optim",
                "find.mf","attachMostRecentStrengths", "TMDensity", "BTDensity",
                "pickTMStrength", "pickBTStrength", "simHomeWin", "generateTeams","generateSchedule", 
-
                "generateConference","generateNonConference", "generateTeamSchedule", "generateSeasonResults",
-        "predictionPercentage", "simulate1", "findMSE", "normalizeSample")
+               "predictionPercentage", "simulate1", "findMSE", "normalizeSample","getConferenceMeans", 
+               "pickUnifStrength","analyzeGameBias")
 clusterExport(clust, neededFunc)
 
 parLapply(clust, 1:10, fun = function(i){
