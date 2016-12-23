b15584cb474412ab8d754b019fb0c73efd2d9ed3
Jackson Curtis <jcurtis210@gmail.com>
2016-12-22 17:26:00 -0700

    Made a sample graph

--- Code/Final Code update May_17_16/SimDataLoader.R
+++ Code/Final Code update May_17_16/SimDataLoader.R
@@ -1,7 +1,7 @@
 #SimDataLoader
 
 files<-paste0("Results/",list.files("Results/"))
-type<-list()
+type<-character()
 trueStrengths<-list()
 centeringValues<-list()
 strengths<-list()
@@ -13,11 +13,25 @@ weeklyGameBias<-list()
 for (i in 1:length(names))
 {
   load(names[i])
-  type[[i]]<-dat$TrueStrengthType
+  type<-c(type, dat$TrueStrengthType)
   trueStrengths[[i]]<-dat$TrueStrengths
   centeringValues[[i]]<-dat$centeringValue
   strengths[[i]]<-dat$strengths
   correlations[[i]]<-dat$SpearmanCorrelation
   gameBias[[i]]<-dat$GameBias
   weeklyGameBias[[i]]<-dat$GameBiasByWeek
+}
+rm(dat)
+
+
+
+cols<-as.factor(type)
+plot(correlations[[1]]$BT, type='l',col=cols[1], ylim=c(.2, 1), xlab="Week", ylab="Spearman Correlation", 
+     main = "Correlation with True Rankings")
+
+lines(correlations[[1]]$TM, lty=2, col=cols[1])
+for (i in 2:10)
+{
+  lines(correlations[[i]]$BT, type='l',col=cols[i])
+  lines(correlations[[i]]$TM, lty=2, col=cols[i])
 }
