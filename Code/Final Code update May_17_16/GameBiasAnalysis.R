6882447de34702ab629527cee2a458fa662fb2cc
Jackson Curtis <jcurtis210@gmail.com>
2017-01-05 14:11:20 -0700

    tweaked colors, labels

--- Code/Final Code update May_17_16/GameBiasAnalysis.R
+++ Code/Final Code update May_17_16/GameBiasAnalysis.R
@@ -2,20 +2,21 @@
 
 gameBiasGraph<-function(BTMatrix, TMMatrix, type)
 {
-  plot(1, type='n', main = "Game Bias MSE", ylab="MSE", xlab="Week", ylim=c(0,.1), xlim=c(1,13), xaxt='n')
+  plot(1, type='n', main = "Win Prediction Probability MSE",
+       ylab="Predicted Probability MSE", xlab="Week", ylim=c(0,.1), xlim=c(1,13), xaxt='n')
   legend("topright", legend=c("Bradley-Terry", "Thurstone-Mosteller", "95% CI"), title=paste(type, "True Strengths"),
-         col=c("Red", "Blue", "pink"), lty = c(1,1,2))
+         col=c("Red", "Blue", "firebrick1"), lty = c(1,1,2))
   BTmeans<-apply(BTMatrix, 1, mean) 
   lines(BTmeans, col="Red", type='l')
   BTsds<-apply(BTMatrix, 1, sd)
   serror<-qnorm(.975)*BTsds/sqrt(ncol(BTMatrix))
-  lines(BTmeans+serror, col="pink", lty=2)
-  lines(BTmeans-serror, col="pink", lty=2)
+  lines(BTmeans+serror, col="firebrick1", lty=2)
+  lines(BTmeans-serror, col="firebrick1", lty=2)
   TMmeans<-apply(TMMatrix, 1, mean)
   TMsds<-apply(TMMatrix, 1, sd)
   serror<-qnorm(.975)*TMsds/sqrt(ncol(TMMatrix))
-  lines(TMmeans+serror, lty=2, col="cyan")
-  lines(TMmeans-serror, lty=2, col="cyan")
+  lines(TMmeans+serror, lty=2, col="steelblue1")
+  lines(TMmeans-serror, lty=2, col="steelblue1")
   lines(TMmeans, col="Blue", type='l')
   axis(1,at=1:12,labels=2:13)
   
@@ -29,7 +30,7 @@ for (i in suffix)
   gameBiasMatrix[[i]]$BT<-sapply(weeklyGameBias[[i]], FUN=function(n){n$BT.MSE})
   gameBiasMatrix[[i]]$TM<-sapply(weeklyGameBias[[i]], FUN=function(n){n$TM.MSE})
 }
-
+par(bg="gray90")
 gameBiasGraph(gameBiasMatrix$BradleyTerryGamma$BT, gameBiasMatrix$BradleyTerryGamma$TM, "Bradley-Terry")
 gameBiasGraph(gameBiasMatrix$Beta$BT, gameBiasMatrix$Beta$TM, "Beta")
 gameBiasGraph(gameBiasMatrix$ThurstoneMostellerNormal$BT, gameBiasMatrix$ThurstoneMostellerNormal$TM, "Thurstone-Mosteller")
