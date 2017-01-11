4996fc093383d246e1d80c642267e629e01d21d6
Jackson Curtis <jcurtis210@gmail.com>
2017-01-11 13:53:41 -0700

    adjusted lengths

--- Code/Final Code update May_17_16/SupercomputerSim.R
+++ Code/Final Code update May_17_16/SupercomputerSim.R
@@ -4,7 +4,7 @@ load("MasterFunctionFile.RData")
 Rcpp::sourceCpp("cppFiles.cpp")
 
 library(parallel)
-numCores<-10
+numCores<-20
 clust <- makeCluster(numCores)
 neededFunc<- c("dataconfigure", "LARC.Rank.Football", "BTDensity", "TMDensity",
                "ThurstoneMostellerLARC",   "BradleyTerryLARC", "LARC.Rank", "LARC.Optim",
@@ -15,10 +15,10 @@ neededFunc<- c("dataconfigure", "LARC.Rank.Football", "BTDensity", "TMDensity",
                "pickBetaStrength","analyzeGameBias")
 clusterExport(clust, neededFunc)
 
-parLapply(clust, 1:15, fun = function(i){
+parLapply(clust, 1:30, fun = function(i){
   Rcpp::sourceCpp("cppFiles.cpp")
-  useBT <- i<6
-  useBeta<-i>10
+  useBT <- i<=10
+  useBeta<-i>20
   dat<-simulate1(useBT, useBeta)
   save(dat, file=paste0("~/LARC/LARC/Code/Final\ Code\ update\ May_17_16/Results/season",i, ".rdata"))
   i})
