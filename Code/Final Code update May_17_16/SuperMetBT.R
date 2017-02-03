944b16b8e4377fcba6d99ad60e0b86e87cee0a8c
Jackson Curtis <jcurtis210@gmail.com>
2017-02-03 07:29:43 -0700

    updated TM

--- Code/Final Code update May_17_16/SuperMetBT.R
+++ Code/Final Code update May_17_16/SuperMetBT.R
@@ -5,13 +5,14 @@ Rcpp::sourceCpp("cppFiles.cpp")
 
 load("Stripped2016FootballData.RData")
 
-dat<-MetHast(logBTDensity, winsMatrix = stripped2016data[[15]][[1]]$WinsVersus, rnormSD=.045, useTimer = T, time=2260)
+dat<-MetHast(logBTDensity, winsMatrix = stripped2016data[[15]][[1]]$WinsVersus, rnormSD=.06, useTimer = T, time=2260)
 print("num rejects: " )
       print(dat[[2]])
       print("Dimensions")
+      print(rnormSD)
 print(dim(dat[[1]]))
 
 finalBT<-handleBurnIn(dat[[1]], 3000)
 rm(dat)
-finalBT<-useEvery(finalBT, 200)
+finalBT<-useEvery(finalBT, 300)
 save(finalBT, file="MetHastResultsActualBT.RData")
--- Code/Final Code update May_17_16/SuperMetTM.R
+++ Code/Final Code update May_17_16/SuperMetTM.R
@@ -5,7 +5,7 @@ Rcpp::sourceCpp("cppFiles.cpp")
 
 load("Stripped2016FootballData.RData")
 
-dat<-MetHast(logTMDensity, winsMatrix = stripped2016data[[15]][[1]]$WinsVersus, rnormSD=.06, useTimer = T, time=2260)
+dat<-MetHast(logTMDensity, winsMatrix = stripped2016data[[15]][[1]]$WinsVersus, rnormSD=.065, useTimer = T, time=2260)
 print("num rejects: " )
       print(dat[[2]])
       print("Dimensions")
@@ -13,5 +13,5 @@ print(dim(dat[[1]]))
 
 finalTM<-handleBurnIn(dat[[1]], 3000)
 rm(dat)
-finalTM<-useEvery(finalTM, 200)
+finalTM<-useEvery(finalTM, 300)
 save(finalTM, file="MetHastResultsTMActual.RData")
