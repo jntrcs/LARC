22cd5ba2ca804033e0d0248e1af15bc8fb056aa8
Jackson Curtis <jcurtis210@gmail.com>
2017-03-23 14:46:37 -0600

    analyzed MH results

--- Code/Final Code update May_17_16/CorrelationAnalysis.R
+++ Code/Final Code update May_17_16/CorrelationAnalysis.R
@@ -1,5 +1,11 @@
 ###CorrelationAnalysis.R
-#Create the Spearman correlation graphs shown in the paper
+suffix<-c("BradleyTerryGamma", "Beta","ThurstoneMostellerNormal", "ExtremeBT")
+
+###CODE FOR ONE CORRELATION GRAPH--outdated
+
+
+
+##Code for 3 correlation graphs
 
 
 correlationPlot<-function(BTmatrix, TMmatrix, BTMMatrix, TMMMatrix, title, ymax=1, ymin=.2)
@@ -12,55 +18,44 @@ correlationPlot<-function(BTmatrix, TMmatrix, BTMMatrix, TMMMatrix, title, ymax=
   lines(tmMean, lty=1, lwd=2, col="Firebrick")
   lines(apply(BTMMatrix, 1, mean), lwd=2, lty=2, col="gray")
   lines(apply(TMMMatrix,1,mean), lwd=2, lty=2, col= "Red")
-
+  #qboundsBT<-apply(BTmatrix, 1, FUN=function(d){quantile(d, c(0.025, .975))})
+  #lines(qboundsBT[1,], lty=1, lwd=1)
+  #lines(qboundsBT[2,], lty=1, lwd=1)
+  #qboundsTM<-apply(TMmatrix, 1, FUN=function(d){quantile(d, c(0.025, .975))})
+  #lines(qboundsTM[1,], lty=2, lwd=1, col="gray")
+  #lines(qboundsTM[2,], lty=2, lwd=1, col="gray")
+  #sdsBT<-apply(BTmatrix, 1, sd)
+  #lowerBoundsBT<-btMean-qnorm(.975)*sdsBT/sqrt(ncol(BTmatrix))
+  #lines(lowerBoundsBT, lty=2, col="Red")
+  #upperBoundsBT<-btMean+qnorm(.975)*sdsBT/sqrt(ncol(BTmatrix))
+  #lines(upperBoundsBT, lty=2, col="Red")
+  #sdsTM<-apply(TMmatrix, 1, sd)
+  #lowerBoundsTM<-tmMean-qnorm(.975)*sdsTM/sqrt(ncol(TMmatrix))
+  #lines(lowerBoundsTM, lty=2, col="Blue")
+  #upperBoundsTM<-tmMean+qnorm(.975)*sdsTM/sqrt(ncol(TMmatrix))
+  #lines(upperBoundsTM, lty=2, col="Blue")
   legend("bottomright", legend=c("Bradley-Terry Mode", "Thurstone-Mosteller Mode", "BT Mean", "TM Mean"),
          title=paste(title, "Underlying Strengths"), lty=c(1,1,2,2), lwd=c(2), col=c("black", 
                                                                                      "firebrick", "Gray", "Red"))
 }
 
-correlationPlot<-function(BTmatrix, TMmatrix, title, ymax=1, ymin=.2)
-{
-  plot(1, type='n', main = paste("Rank Correlation with", title,  "True Strengths"), ylab="Spearman Rank Correlation Coefficent",
-       xlab="Week", ylim=c(ymin, ymax), xlim=c(0,14)) 
-  btMean<-apply(BTmatrix, 1, mean)
-  tmMean<-apply(TMmatrix, 1, mean)
-  lines(btMean, lty=1, lwd=2)
-  lines(tmMean, lty=2, lwd=2, col="gray")
-  qboundsBT<-apply(BTmatrix, 1, FUN=function(d){quantile(d, c(0.025, .975))})
-  lines(qboundsBT[1,], lty=1, lwd=1)
-  lines(qboundsBT[2,], lty=1, lwd=1)
-  qboundsTM<-apply(TMmatrix, 1, FUN=function(d){quantile(d, c(0.025, .975))})
-  lines(qboundsTM[1,], lty=2, lwd=1, col="gray")
-  lines(qboundsTM[2,], lty=2, lwd=1, col="gray")
-  sdsBT<-apply(BTmatrix, 1, sd)
-  lowerBoundsBT<-btMean-qnorm(.975)*sdsBT/sqrt(ncol(BTmatrix))
-  #lines(lowerBoundsBT, lty=2, col="Red")
-  upperBoundsBT<-btMean+qnorm(.975)*sdsBT/sqrt(ncol(BTmatrix))
-  #lines(upperBoundsBT, lty=2, col="Red")
-  sdsTM<-apply(TMmatrix, 1, sd)
-  lowerBoundsTM<-tmMean-qnorm(.975)*sdsTM/sqrt(ncol(TMmatrix))
-  #lines(lowerBoundsTM, lty=2, col="Blue")
-  upperBoundsTM<-tmMean+qnorm(.975)*sdsTM/sqrt(ncol(TMmatrix))
-  #lines(upperBoundsTM, lty=2, col="Blue")
-  legend("bottomright", legend=c("Bradley-Terry Rankings", "Thurstone-Mosteller Rankings", "95% Quantiles"),
-         title=paste(title, "Underlying Strengths"), lty=c(1,2,1), lwd=c(2,2,1))
-  rect(-1,-1,4,1,col = rgb(0.5,0.5,0.5,1/4), border=NA)
-  legend("topleft", legend="Shaded area represents non-conference weeks", cex=.7)
-}
-
 suffix<-c("BradleyTerryGamma", "Beta","ThurstoneMostellerNormal", "ExtremeBT")
 BTMatrix<-list()
 TMMatrix<-list()
+BTMMatrix<-list()
+TMMMatrix<-list()
 for (i in suffix)
 {
   BTMatrix[[i]]<-sapply(correlations[[i]], FUN = function(n) {n$BT})
   TMMatrix[[i]]<-sapply(correlations[[i]], FUN = function(n) {n$TM})
+  BTMMatrix[[i]]<-sapply(correlations[[i]], FUN = function(n) {n$BTMean})
+  TMMMatrix[[i]]<-sapply(correlations[[i]], FUN = function(n) {n$TMMean})
 }
 par(mfrow=c(1,1))
 
-correlationPlot(BTMatrix$Beta, TMMatrix$Beta, "Beta", ymax=.7)
-correlationPlot(BTMatrix$BradleyTerryGamma, TMMatrix$BradleyTerryGamma, "Bradley-Terry", ymax=.85, ymin=.4)
-correlationPlot(BTMatrix$ThurstoneMostellerNormal, TMMatrix$ThurstoneMostellerNormal, "Thurstone-Mosteller",
-                ymax=.95, ymin=.4)
-correlationPlot(BTMatrix$ExtremeBT, TMMatrix$ExtremeBT, "Extreme-BT",
-                ymax=.95, ymin=.4)
+correlationPlot(BTMatrix$Beta, TMMatrix$Beta, BTMMatrix$Beta, TMMMatrix$Beta, "Beta", ymax=.7)
+correlationPlot(BTMatrix$BradleyTerryGamma, TMMatrix$BradleyTerryGamma, BTMMatrix$BradleyTerryGamma, TMMMatrix$BradleyTerryGamma, "Bradley-Terry", ymax=.85, ymin=.3)
+correlationPlot(BTMatrix$ThurstoneMostellerNormal, TMMatrix$ThurstoneMostellerNormal, BTMMatrix$ThurstoneMostellerNormal,
+                TMMMatrix$ThurstoneMostellerNormal, "Thurstone-Mosteller",ymax=.9, ymin=.4)
+correlationPlot(BTMatrix$ExtremeBT, TMMatrix$ExtremeBT, BTMMatrix$ExtremeBT, TMMMatrix$ExtremeBT, "Extreme-BT",
+                ymax=.9, ymin=.4)
--- Code/Final Code update May_17_16/SimDataLoader.R
+++ Code/Final Code update May_17_16/SimDataLoader.R
@@ -1,19 +1,18 @@
-#SimDataLoader
-##Loads all the data files in the Results subfolder into memory
-
 load("MasterFunctionFile.RData")
-files<-paste0("Results/",list.files("Results/"))
+files<-paste0("MHResults/",list.files("MHResults/"))
 type<-factor()
-trueStrengths<-list(BradleyTerryGamma=vector("list", 1),Beta=vector("list", 1), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
-centeringValues<-list(BradleyTerryGamma=vector("list", 1),Beta=vector("list", 1), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
-strengths<-list(BradleyTerryGamma=vector("list", 1),Beta=vector("list", 1), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
-correlations<-list(BradleyTerryGamma=vector("list", 1),Beta=vector("list", 1), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
-gameBias<-list(BradleyTerryGamma=vector("list", 1),Beta=vector("list", 1), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
-weeklyGameBias<-list(BradleyTerryGamma=vector("list", 1),Beta=vector("list", 1), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+trueStrengths<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+centeringValues<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+strengths<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+CISize<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+acceptanceRates<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+correlations<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+gameBias<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
+weeklyGameBias<-list(BradleyTerryGamma=vector("list", 5),Beta=vector("list", 5), ThurstoneMostellerNormal=vector("list", 5), ExtremeBT=vector("list",5))
 disparity<-list(BradleyTerryGamma=numeric(),Beta=numeric(), ThurstoneMostellerNormal=numeric(), ExtremeBT=numeric())
 
 indices<-rep(0,4) # BT then TM then Beta then EXTREMEBETA
-#for (i in 1:length(files))
+for (i in 1:length(files))
 {
   load(files[i])
   ifelse(dat$TrueStrengthType=="BradleyTerryGamma", index<-1,
@@ -24,15 +23,17 @@ indices<-rep(0,4) # BT then TM then Beta then EXTREMEBETA
   trueStrengths[dat$TrueStrengthType][[1]][[a]]<-dat$TrueStrengths
   centeringValues[dat$TrueStrengthType][[1]][[a]]<-dat$centeringValue
   strengths[dat$TrueStrengthType][[1]][[a]]<-dat$strengths
+  CISize[dat$TrueStrengthType][[1]][[a]]<-dat$CISize
+  acceptanceRates[dat$TrueStrengthType][[1]][[a]]<-dat$AcceptanceRate
   correlations[dat$TrueStrengthType][[1]][[a]]<-dat$SpearmanCorrelation
   gameBias[dat$TrueStrengthType][[1]][[a]]<-dat$GameBias
-    weeklyGameBias[dat$TrueStrengthType][[1]][[a]]<-dat$GameBiasByWeek
-    disparity[[dat$TrueStrengthType]][[a]]<-dat$disparityScore
-        
-  }
+  weeklyGameBias[dat$TrueStrengthType][[1]][[a]]<-dat$GameBiasByWeek
+  disparity[[dat$TrueStrengthType]][[a]]<-dat$disparityScore
+  
+}
 rm(dat, a, i, index, indices, files)
 type<-as.factor(type)
 
-#save(type, trueStrengths, centeringValues, strengths, correlations, gameBias, weeklyGameBias, disparity, file="SimulationResults.RData")
+#save(type, trueStrengths, centeringValues, strengths, correlations, rejectionRate, gameBias, weeklyGameBias, disparity, file="SimulationResults.RData")
 #rm(type, trueStrengths, centeringValues, strengths, correlations, gameBias)
 #load("SimulationResults.RData")
